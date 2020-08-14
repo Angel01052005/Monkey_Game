b057976a28e4a452045debbc1160350eead05352
Angel01052005 <69529762+Angel01052005@users.noreply.github.com>
2020-08-14 14:40:15 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+//Global Variables
+
+
+
+function preload(){
+  
+}
+
+
+function setup() {
+  createCanvas(600,300);
+  monkey = createSprite(200,200,20,20);
+}
+
+
+function draw(){
+ background(255); 
+  drawSprites();
+}
